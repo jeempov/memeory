9d341188ca178ef61a0f8bbc353426d406b1ab0f
Jim Pov <jimpov@gmail.com>
2021-04-23 11:40:34 -0400

    double decker bby

--- script.js
+++ script.js
@@ -52,25 +52,24 @@ function Card (value, isFlipped, img) {
     this.isFlipped = isFlipped;
     this.img = img;
     this.id = -1;
-}
-​
-Card.prototype.html = function() {
+}Card.prototype.html = function() {
 	return `<div class="card"><img src="${ this.img }" data-id="${ this.id }"></div>`;
 }
 
 Card.prototype.backHtml = function() {
 	return `<div class="card"><img src="https://images-na.ssl-images-amazon.com/images/I/61AGZ37D7eL._AC_SL1039_.jpg" data-id="${ this.id }"></div>`;
 }
-​
+
 function Deck (cards) {
 		this.cards = cards || [];
     for (let i = 0; i < this.cards.length; i++) {
     	this.cards[i].id = i;
     }
 }
-​
+
 Deck.prototype.add = function (aCard) {
     this.cards.push(aCard);
+    this.cards[this.cards.length - 1].id = this.cards.length - 1;
 }
 
 Deck.prototype.draw = function () {
@@ -87,30 +86,26 @@ Deck.prototype.draw = function () {
     document.querySelector('#board').innerHTML += html;
   }
 };
-​
 document.querySelector('#board').addEventListener('click', function (event) {
 	let cardId = event.target.dataset.id;
-  deck.cards[cardId].isFlipped = !deck.cards[cardId].isFlipped;
-  deck.draw();
+  myDeck.cards[cardId].isFlipped = !myDeck.cards[cardId].isFlipped;
+  myDeck.draw();
 });
 
-​
 const myDeck = new Deck();
-​for (let i = 0; i <= CARDS.length; i++) {
-  myDeck.add(CARDS[i])
+for (let i = 0; i < CARDS.length; i++) {
+  let mycard = new Card(CARDS[i].value, true, CARDS[i].img);
+  myDeck.add(mycard)
+  let mycard2 = new Card(CARDS[i].value, true, CARDS[i].img);
+  myDeck.add(mycard2)
 }
-
-
-
-
 
 //deck.add(something);
 myDeck.draw();
-​
+
 //https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/conditionals
 //switch () {
 //    case card1
-   
 //}
-​
+
 //if bomb then boom
